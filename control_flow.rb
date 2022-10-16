eff2b196aad7d046a77245de350f4b066ed9bdb6
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-10-17 00:17:20 +0300

    control flow

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,53 @@
+# require "pry"
 def admin_login(username, password)
   # your code here
+  if username == "admin" && password == "12345"
+    return "Access granted"
+  elsif username == "ADMIN" && password == "12345"
+    return "Access granted"
+  else
+    return "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
   # your code here
+  if temperature < 40
+    return "It's brisk out there!"
+  elsif temperature > 40 && temperature < 65
+    return "It's a little chilly out there!"
+  elsif temperature > 85
+    return "It's too dang hot out there!"
+  else
+    return "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
   # your code here
+  if num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  elsif num % 5 === 0 && num % 3 === 0
+    return "FizzBuzz"
+  else
+    return num
+  end
 end
 
 def calculator(operation, num1, num2)
   # your code here
+  if operation == "+"
+    return num1 + num2
+  elsif operation == "-"
+    return num1 - num2
+  elsif operation == "*"
+    return num1 * num2
+  elsif operation == "/"
+    return num1 / num2
+  elsif operation == "nope"
+    puts "Invalid operation!"
+  end
 end
 
